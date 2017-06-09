4aaa1a3669c8d7b4dcea3fcd0bee04406cfe6571
kev-null <kevino175@gmail.com>
2017-06-09 13:33:36 +0100

    Refactor
    Pretty big refactor here:
    
    * Now use promises instead of callbacks in most places
    
    * Supports retries for failed chunk gets
    
    * Now uses es6-promise-pools for limiting the concurrent queries

--- get_file.js
+++ get_file.js
@@ -1,7 +1,7 @@
 var DHT = require('bittorrent-dht')
-async = require("async");
-var fs = require('fs');
-
+//async = require("async");
+var PromisePool = require('es6-promise-pool');
+var promiseRetry = require('promise-retry');
 commandLineArgs = require('command-line-args');
 const optionDefinitions = [{
         name: 'password',
@@ -14,6 +14,11 @@ const optionDefinitions = [{
         type: String
     },
     {
+        name: 'outputfile',
+        alias: 'o',
+        type: String
+    },
+    {
         name: 'joblimit',
         alias: 'j',
         type: Number,
@@ -22,40 +27,115 @@ const optionDefinitions = [{
 ];
 
 options = commandLineArgs(optionDefinitions);
-fs.readFile(options['hashfile'], function(err, data) {
-  if (err) throw err;
-  var hash_json = data;
-  var hashes = JSON.parse(hash_json);
-  //console.log(new Buffer(hashes[0][1]['data'], 'hex'));
 
-  var chunks = [];
-  async.eachLimit(hashes, options['joblimit'],
-      function(hash, callback) {
-          var dht = new DHT({bootstrap: true});
-          console.log('getting ' + hash[0])
-          dht.get(new Buffer(hash[1]['data'], 'hex'), function(err, value) {
-              //if(err) throw err;
-              console.log(value.v)
-              chunks.push([hash[0], value.v])
-              dht.destroy();
-              callback();
+readFile(options['hashfile'])
+  .then(function(data) {
+    // carry on
+    var hash_json = data;
+    hashes = JSON.parse(hash_json);
+    //console.log(hashes);
+    //process.exit();
+    chunks = [];
+
+    var count = 0
+    var promiseProducer = function () {
+      //console.log('COUNT: ', count)
+      if (count < hashes.length) {
+        return promiseRetry(function (retry, number) {
+          console.log('attempt number', number);
+          var chunkPromise = getChunk(hashes[count][0], new Buffer(hashes[count][1]['data'], 'hex'))
+          .catch(retry);
+          count++;
+          return chunkPromise;
+        }, [3])
+        .then(function (value) {
+          //count++;
+          return;
+        }, function (err) {
+          console.log('[!] Failed to load chunk. File may be incomplete.')
+        });
+      } else {
+        return null
+      }
+    }
+    const pool = new PromisePool(promiseProducer, options['joblimit'])
+
+    pool.start()
+      .then(() => {
+        console.log('get file complete');
+        //console.log(chunks);
+        chunks.sort(sortFunction);
+        //console.log(chunks);
+        var recovered_file = new Buffer(0);
+        //console.log(chunks);
+        for(chunk_number in chunks) {
+          //console.log(chunks[chunk_number][1])
+          recovered_file = Buffer.concat([recovered_file, chunks[chunk_number][1]])
+        }
+        writeFile(options['outputfile'], recovered_file)
+          .then(() => {
+            console.log('file '+options['outputfile']+' successfully recovered');
           })
-      },
-      function() {
-          console.log('get file complete');
-          chunks.sort(sortFunction);
-          var recovered_file = new Buffer(0);
-          //console.log(chunks);
-          for(chunk_number in chunks) {
-            //console.log(chunks[chunk_number][1])
-            recovered_file = Buffer.concat([recovered_file, chunks[chunk_number][1]])
-          }
-          fs.writeFile(options['hashfile']+'.jpg', recovered_file, function(err) {
-            if(err) throw err;
+          .catch((err) => {
+            throw err;
           })
       })
+      .catch((err) => {
+        throw err;
+      })
+  })
+  .catch(function(err) {
+    throw err;
+  })
 
-});
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    var fs = require('fs');
+    fs.readFile(options['hashfile'], (err, data) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+}
+
+function writeFile(filename, data) {
+  return new Promise((resolve, reject) => {
+    var fs = require('fs');
+    fs.writeFile(filename, data, (err) => {
+      if(err) {
+        console.log('[!] Failed to get chunk: ', seq);
+        reject(err);
+      } else {
+        resolve();
+      }
+    })
+  })
+}
+
+function getChunk(seq, hash) {
+    return new Promise(function(resolve, reject) {
+        var dht = new DHT();
+        console.log('getting '+seq+':');
+        console.log(hash);
+        dht.get(hash, (err, value) => {
+          if (err || value == null) {
+            if(value == null) {
+              err = 'no value';
+            }
+            reject(err);
+          } else {
+              dht.destroy();
+              console.log('Got Chunk '+ seq + ':');
+              console.log(value.v);
+              chunks.push([seq, value.v]);
+              resolve(value);
+          }
+        })
+    });
+}
 
 function sortFunction(a, b) {
     if (a[0] === b[0]) {
